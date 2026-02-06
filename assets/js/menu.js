7e83840fb26c899b7786864a8fa3bf7413b44c82
Lari <larigithub@gmail.com>
2026-02-06 15:37:34 -0300

    feat: add responsive navbar interactions

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,7 +1,20 @@
 const hamburger = document.getElementById("hamburger");
 const menuMobile = document.getElementById("menuMobile");
+const register = document.getElementById("btn-register");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   menuMobile.classList.toggle("active");
+});
+
+register.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const confirmRegistration = confirm("Button don't have functionality yet!");
+
+  if (confirmRegistration) {
+    confirm("User clicked Ok");
+  } else {
+    confirm("User clicked cancel");
+  }
 });
